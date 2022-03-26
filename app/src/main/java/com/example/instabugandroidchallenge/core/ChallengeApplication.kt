c5f20799ae0389a1038ab45f9df809c286ed051b
karem-ata <karem.atta@tooli.tv>
2022-03-27 01:41:50 +0200

    add core classes

---
+++ app/src/main/java/com/example/instabugandroidchallenge/core/ChallengeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.instabugandroidchallenge.core
+
+import android.app.Application
+
+class ChallengeApplication: Application() {
+    val appContainer = AppContainer()
+}
---
+++ app/src/main/java/com/example/instabugandroidchallenge/core/EspressoIdlingResource.kt
@@ -0,0 +1,20 @@
+package com.example.instabugandroidchallenge.core
+
+import androidx.test.espresso.idling.CountingIdlingResource
+
+public object EspressoIdlingResource {
+
+    private const val RESOURCE = "GLOBAL"
+
+    @JvmField val countingIdlingResource = CountingIdlingResource(RESOURCE)
+
+    fun increment() {
+        countingIdlingResource.increment()
+    }
+
+    fun decrement() {
+        if (!countingIdlingResource.isIdleNow) {
+            countingIdlingResource.decrement()
+        }
+    }
+}
