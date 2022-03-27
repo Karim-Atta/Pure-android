ea570a156c0a47e50c20f8a1754b5ad2c0e25709
karem-ata <karem.atta@tooli.tv>
2022-03-27 02:04:11 +0200

    add app container

---
+++ app/src/main/java/com/example/instabugandroidchallenge/core/AppContainer.kt
@@ -0,0 +1,7 @@
+package com.example.instabugandroidchallenge.core
+
+import com.example.instabugandroidchallenge.countedwords.presentation.di.CountedWordsContainer
+
+class AppContainer {
+    var countedWordsContainer: CountedWordsContainer? = null
+}
